90647ea851f32cde29b49fc87ec7f0e9d3410c8c
qiaor1 <qiaor1@uci.edu>
2019-02-21 12:05:22 -0800

    Update PlayerScript.cs

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
+using UnityEngine.Events;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    UnityEvent in_water;
+    UnityEvent on_vine;
+    UnityEvent wind;
     public Rigidbody2D m_rb;
+ 
+    public Vector3 m_rb_vel;
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
+        m_rb_vel = GetComponent<Rigidbody2D>().velocity;
     }
 
     // Update is called once per frame
@@ -16,16 +23,20 @@ public class PlayerScript : MonoBehaviour
     {
         
     }
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)//maybe stay2d
     {
         if(collision.CompareTag("Water"))
         {
-            //m_rb.AddRelativeForce(Vector3.down);
+            m_rb.AddRelativeForce(Vector3.down-m_rb_vel);
         }
         else if (collision.CompareTag("Vine"))
         {
             //move up or down bu W&S
             //inst 2 coillders to stop the player to go out off the vine?
+        }
+        else if(collision.CompareTag("Wind"))
+        {
+
         }
     }
 }
