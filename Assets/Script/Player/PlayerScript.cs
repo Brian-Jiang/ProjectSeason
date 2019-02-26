109941de9775a26664b4891ad42cb3f320fdd89f
Spitwater <Spitwater@users.noreply.github.com>
2019-02-25 17:00:38 -0800

    platform

--- Assets/Script/Player/PlayerScript.cs
+++ Assets/Script/Player/PlayerScript.cs
@@ -151,4 +151,20 @@ public class PlayerScript : MonoBehaviour
             enabledJump = true;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Platform"))
+        {
+             this.transform.parent = collision.collider.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Platform"))
+        {
+            this.transform.parent = null;
+        }
+    }
 }
