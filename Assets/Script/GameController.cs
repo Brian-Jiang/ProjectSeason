e74cc51e03545ea67ed9cfc006b1466910869dcb
Spitwater <Spitwater@users.noreply.github.com>
2019-02-26 15:20:03 -0800

    player

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -28,15 +28,9 @@ public class GameController : MonoBehaviour
 
 
         DontDestroyOnLoad(this);
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(m_player == null)
-        {
-            m_player = GameObject.FindGameObjectWithTag("Player");
-        }
+        m_player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     // Update is called once per frame
@@ -62,8 +56,8 @@ public class GameController : MonoBehaviour
         SceneManager.LoadScene("Prototype");
     }
 
-    private void OnLevelWasLoaded(int level)
+    public GameObject GetPlayer()
     {
-        
+        return m_player;
     }
 }
