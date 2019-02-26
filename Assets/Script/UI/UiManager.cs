64522e27577c0c9d1d38d5dc2d877d986d13699f
Erin Luo <haolinl4@uci.edu>
2019-02-25 21:51:58 -0800

    start menu 1.0

---
+++ Assets/Script/UI/UiManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartButton()
+    {
+        //SceneManager.LoadScene("Level1");
+    }
+
+    // Update is called once per frame
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
