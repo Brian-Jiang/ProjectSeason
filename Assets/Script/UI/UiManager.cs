8d86c49027c97fceb281837308e6362dc21f7ebe
Erin Luo <haolinl4@uci.edu>
2019-02-25 22:42:42 -0800

    pause menu 1.0

--- Assets/Script/UI/UiManager.cs
+++ Assets/Script/UI/UiManager.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UiManager: MonoBehaviour
 {
     // Start is called before the first frame update
     public void StartButton()
     {
-        //SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Prototype");
     }
 
     // Update is called once per frame
