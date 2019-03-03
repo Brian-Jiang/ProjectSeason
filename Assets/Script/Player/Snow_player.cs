120d995b3706a6ecf2f6a2cddbfad0113ee632a6
qiaor1 <qiaor1@uci.edu>
2019-03-03 10:08:32 -0800

    Snow_ball

---
+++ Assets/Script/Player/Snow_player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow_player : MonoBehaviour
+{
+    private Rigidbody2D m_rb;
+    [SerializeField] public float go_up_speed;
+    [SerializeField] public Transform player_p;
+    [SerializeField] public float move_speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_rb = GetComponent<Rigidbody2D>();
+        m_rb.gravityScale = 0;
+        player_p = GetComponent<Transform>();
+        m_rb.AddForce(Vector2.up * go_up_speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Move();
+
+       
+    }
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_rb.AddForce(Vector2.left * move_speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_rb.AddForce(Vector2.right*move_speed);
+        }
+        m_rb.velocity = Vector2.up * go_up_speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("NextScene"))
+        {
+            //scene transform here
+        }
+        else if ( collision.CompareTag("Snowball"))
+        {
+            //player life -1
+        }
+    }
+   
+}
