d844416049d9b08ce09aef35c100a5362f5eb024
Erin Luo <haolinl4@uci.edu>
2019-03-03 22:53:45 -0800

    end and pause

--- Assets/Script/Player/Snow_player.cs
+++ Assets/Script/Player/Snow_player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Snow_player : MonoBehaviour
 {
@@ -46,7 +47,7 @@ public class Snow_player : MonoBehaviour
     {
         if ( collision.CompareTag("NextScene"))
         {
-            //scene transform here
+            SceneManager.LoadScene("End"); //load the thank you for playing scene
         }
         else if ( collision.CompareTag("Snowball"))
         {
