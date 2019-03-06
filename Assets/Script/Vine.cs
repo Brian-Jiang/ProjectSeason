8bbe69dd455ab21fb05f13a9a34636307f3d1038
qiaor1 <qiaor1@uci.edu>
2019-03-05 16:31:06 -0800

    pot

---
+++ Assets/Script/Vine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vine : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    public Rigidbody2D vine_force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vine_force = GetComponent<Rigidbody2D>();
+        vine_force.AddForce(Vector2.up * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Stopper"))
+        {
+            vine_force.velocity = Vector2.zero;
+        }
+    }
+}
