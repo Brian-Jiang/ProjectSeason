905db7188516bfc2e6b4c27747260b796ed6b79e
Spitwater <Spitwater@users.noreply.github.com>
2019-03-11 12:04:34 -0700

    tracking

--- Assets/Script/EnemyShooting.cs
+++ Assets/Script/EnemyShooting.cs
@@ -7,6 +7,8 @@ public class EnemyShooting : MonoBehaviour
     [SerializeField] private float att_frq;
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform firep;
+    [SerializeField] private bool trackingbul;
+    private GameObject cur_player;
     private SpriteRenderer m_render;
     private float nextAttack;
  
@@ -14,7 +16,8 @@ public class EnemyShooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //m_control = GameObject.FindGameObjectWithTag("GameCon")
+        cur_player = GameController.instance.GetPlayer();
+        //m_control = GameObject.FindGameObjectWithTag("GameCo")
         m_render = GetComponentInChildren<SpriteRenderer>();
         if (!firep)
             firep = transform;
@@ -23,12 +26,20 @@ public class EnemyShooting : MonoBehaviour
     }
     private void Attack()
     {
-        
-        GameObject bulletobj = Instantiate(bullet, firep.position, transform.rotation);
-        Rigidbody2D m_bulletRB =  bulletobj.GetComponent<Rigidbody2D>();
-        //Debug.Log(bulletobj.transform.position);
-        if (m_bulletRB)
-            m_bulletRB.velocity = new Vector2(x, y).normalized * speed;
+        if (trackingbul == false)
+        {
+            GameObject bulletobj = Instantiate(bullet, firep.position, transform.rotation);
+            Rigidbody2D m_bulletRB = bulletobj.GetComponent<Rigidbody2D>();
+            if (m_bulletRB)
+                m_bulletRB.velocity = new Vector2(x, y).normalized * speed;
+        }
+        else
+        {
+            GameObject bulletobj = Instantiate(bullet, firep.position, transform.rotation);
+            Bullet_Tra bullet_t = bulletobj.GetComponent<Bullet_Tra>();
+            bullet_t.SetTarget(cur_player);
+        }
+
         nextAttack = Time.time + att_frq;
     }
 
