45464a5530c25ed7fcf216807174a95194ba4252
Erin Luo <haolinl4@uci.edu>
2019-03-11 23:59:10 -0700

    noteboard

--- Assets/Script/UI/NoteBoardTrigger.cs
+++ Assets/Script/UI/NoteBoardTrigger.cs
@@ -7,28 +7,29 @@ public class NoteBoardTrigger : MonoBehaviour
     // Start is called before the first frame update
     bool NoteAppear = false;
     public GameObject note;
+    int count;
 
     private void Update()
     {
         //note.SetActive(false);
+        count++;
+        if (count == 600)
+        {
+            note.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         note.SetActive(true);
+        count = 0;
 
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider other)
     {
-        note.SetActive(false);
+        //note.SetActive(false);
     }
 
-
-
-    private void OnTriggerStay(Collider other)
-    {
-        note.SetActive(true);
-    }
 
 }
