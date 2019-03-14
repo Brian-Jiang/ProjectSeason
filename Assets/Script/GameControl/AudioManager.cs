d530cdfc3a4b58600cb61bce15894476f3ed4019
Erin Luo <haolinl4@uci.edu>
2019-03-13 17:46:21 -0700

    audio

---
+++ Assets/Script/GameControl/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip JumpClip;
+    public AudioSource JumpSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JumpSource.clip = JumpClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (PlayerScript.is jump)
+        {
+            JumpSource.Play();
+        }
+    }
+}
